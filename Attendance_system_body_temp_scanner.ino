0b4cb321a624dd8df5c4fdfd787b5d92532f11b3
pacianoseth <feintimg@gmail.com>
2023-05-24 22:49:26 +0800

    added README.md

--- Attendance_system_body_temp_scanner.ino
+++ Attendance_system_body_temp_scanner.ino
@@ -1,7 +1,17 @@
- 
-//Prateek
-//www.prateeks.in
 
+
+// * Typical pin layout used:
+// * -----------------------------------------------------------------------------------------
+// *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+// *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+// * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+// * -----------------------------------------------------------------------------------------
+// * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+// * SPI SS      SDA(SS)      10            53        D10        10               10
+// * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+// * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+// * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+// */
 #include <Wire.h>
 #include <SPI.h>
 #include <MFRC522.h>
@@ -11,33 +21,39 @@
 #include <DS3231.h>
 #include <SD.h>
 
-#define CS_RFID 10
-#define RST_RFID 9
-#define CS_SD 6
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
 
+#define CS_SD           6
 File myFile;
+
 
 
 DS3231  rtc(SDA, SCL);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 SoftwareSerial sim(7, 8);
-Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-MFRC522 rfid(CS_RFID, RST_RFID); 
-
 String _buffer;
-String uidString;
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 int relay = 2;
 
-void setup() {
-  Serial.begin(9600);
-  _buffer.reserve(100);
+void setup() 
+{
+  Serial.begin(9600);   // Initialize serial communications with the PC 
+  
+  _buffer.reserve(50);
   sim.begin(9600);
-  while(!Serial); 
-  SPI.begin(); 
-  rfid.PCD_Init(); 
-  rfid.PCD_DumpVersionToSerial(); 
+  delay(1000);
+  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+
+
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+  
+  
 
   rtc.begin();
   lcd.begin(16,2);
@@ -45,211 +61,212 @@ void setup() {
   lcd.backlight();
   mlx.begin();
 
-  Serial.print("Initializing SD card...");
-  delay(3000);
-  lcd.clear();
-  if(!SD.begin(CS_SD)) {
-    Serial.println("initialization failed!");
-    lcd.print("Initializing ");
-    lcd.setCursor(0, 1);
-    lcd.print("failed!");
-    return;
-  }
-  Serial.println("initialization done.");
 
+    // Setup for the SD card
+  Serial.print("Initializing SD card...");
+  if(!SD.begin(CS_SD)) 
+    {
+    Serial.println("initialization failed!");
+    return;
+    }
+    Serial.println("initialization done.");
+
+  //rtc.setDOW(SATURDAY);
+  //rtc.setTime(11, 05, 0);
+  //rtc.setDate(20, 5, 2023);
   pinMode(relay, OUTPUT);
-  //digitalWrite(relay, HIGH);
+  digitalWrite(relay, HIGH); //setting relay at LOW, NO()
+
 }
-void loop() {
+
+void loop() 
+{
+  //temperature
+  float temp = mlx.readObjectTempC();
+  //temperature
+  //Real time clock 
+  String time = rtc.getTimeStr();
+  String date = rtc.getDateStr();
+  //Real time clock
 
   // Look for new cards
-  if ( ! rfid.PICC_IsNewCardPresent()) {
-    return;
-  }
-
-    // Select one of the cards
-  if ( ! rfid.PICC_ReadCardSerial()) {
-    return;
-  }
-   rfid.PICC_ReadCardSerial();
-  lcd.clear();
-  Serial.print("Tag UID: ");
-  lcd.print("Tag UID: ");
-  uidString = String(rfid.uid.uidByte[0]) + " " + String(rfid.uid.uidByte[1]) + " " + 
-    String(rfid.uid.uidByte[2]) + " " + String(rfid.uid.uidByte[3]);
-Serial.println(uidString);
-  lcd.setCursor(0, 1);
-  lcd.print(uidString);
-  delay(2000);
-
-
-
-  //Serial.println(uidString);
-  //Serial.println("Sending Message");
- if (uidString == "74 175 72 23") //change here the UID of the card/cards that you want to give access
-{
-  
-   //temperature
-  float temp = mlx.readObjectTempC();
-  //temperature
-  //Real time clock 
-  String time = rtc.getTimeStr();
-  String date = rtc.getDateStr();
-  //Real time clock
-  String number = "09655364792";
-
-  //digitalWrite(relay, LOW);
-  
-  Serial.println("Card Read and authenticated");
-  //LCD
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Temperature: ");
-  lcd.setCursor(0, 1);
-  lcd.print(temp);
-  //LCD
-
-  //Serial Monitor
-  Serial.print("Temperature: ");
-  Serial.println(temp);
-  Serial.print("Time: ");
-  Serial.println(time);
-  Serial.print("Date: ");
-  Serial.println(date);
-  //Serial Monitor
-
-  delay(2000);
-  //GSM send message
-  sim.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
-  delay(200);
-  //Serial.println ("Set SMS Number");
-  sim.println("AT+CMGS=\"" + number + "\"\r"); //Mobile phone number to send message
-  delay(200);
-  sim.print("Seth" );
-  //sim.println(" arrived" );
-  //sim.println(" temp");
-  delay(200);
-
-  //sim.print("Temp: ");
-  //sim.println(temp);
-  //sim.println("Time: " + time);
-  //sim.println("Date: " + date);    
-  //sim.print("T: ");
-  sim.println((char)26);// ASCII code of CTRL+Z
-  //GSM send message
-
-  delay(2000);
-  digitalWrite(relay, HIGH);
-      //Data Log
-  //digitalWrite(SS_PIN, HIGH);
-  digitalWrite(CS_SD,LOW);
-  myFile = SD.open("DataLog.txt", FILE_WRITE);
-    if (myFile) {
-    Serial.println("File opened ok");
-    delay(2000);
-    myFile.print("Name: Seth");   
-    myFile.print(" temp: ");
-    myFile.print(temp);
-    myFile.print(" Time: ");
-    myFile.print(time);
-    myFile.print(" Date: ");
-    myFile.println(date);
-    delay(2000);
-    lcd.clear();
-    myFile.close(); // close the file
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+    {
+      return;
     }
-   else {
-    Serial.println("error opening Data Log.txt");  
-    myFile.close();
-  }
- digitalWrite(CS_SD,HIGH);
-  //Data Log
-}
-
-if (uidString == "35 190 192 51")
-{
-     //temperature
-  float temp = mlx.readObjectTempC();
-  //temperature
-  //Real time clock 
-  String time = rtc.getTimeStr();
-  String date = rtc.getDateStr();
-  //Real time clock
-  String number = "09277412790";
-
-  Serial.println("Card Read and authenticated");
-  //LCD
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Temperature: ");
-  lcd.setCursor(0, 1);
-  lcd.print(temp);
-  //LCD
-
-  //Serial Monitor
-  Serial.print("Temperature: ");
-  Serial.println(temp);
-  Serial.print("Time: ");
-  Serial.println(time);
-  Serial.print("Date: ");
-  Serial.println(date);
-  //Serial Monitor
-
-  delay(2000);
-  //GSM send message
-  sim.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
-  delay(200);
-  //Serial.println ("Set SMS Number");
-  sim.println("AT+CMGS=\"" + number + "\"\r"); //Mobile phone number to send message
-  delay(200);
-  sim.print("Aden" );
-  sim.println(" arrived" );
-  //sim.println(" temp");
-  delay(200);
-
-  //sim.print("Temp: ");
-  sim.println(temp);
-  sim.println("Time: " + time);
-  sim.println("Date: " + date);    
-  //sim.print("T: ");
-  sim.println((char)26);// ASCII code of CTRL+Z
-   delay(2000);
-  //GSM send message
-
-  delay(2000);
-      //Data Log
-  //digitalWrite(SS_PIN, HIGH);
-  digitalWrite(CS_SD,LOW);
-  myFile = SD.open("DataLog.txt", FILE_WRITE);
-    if (myFile) {
-    Serial.println("File opened ok");
-    delay(2000);
-    myFile.print("Name: Aden");   
-    myFile.print(" temp: ");
-    myFile.print(temp);
-    myFile.print(" Time: ");
-    myFile.print(time);
-    myFile.print(" Date: ");
-    myFile.println(date);
-    delay(2000);
-    lcd.clear();
-    myFile.close(); // close the file
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+    {
+      return;
     }
-   else {
-    Serial.println("error opening Data Log.txt");  
-    myFile.close();
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+     mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ";
+     mfrc522.uid.uidByte[i], HEX;
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
- digitalWrite(CS_SD,HIGH);
-  //Data Log
-}
-else
-{
-  Serial.println("Scan RFID");
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Scan RFID");
-}
+  content.toUpperCase();
+  Serial.println(content.substring(1));
+ if (content.substring(1) == "4A AF 48 17") //change here the UID of the card/cards that you want to give access
+  {
+  
+      String number = "09655364792";
+      Serial.println("Card Read and authenticated");
+      //LCD
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Temperature: ");
+      lcd.setCursor(0, 1);
+      lcd.print(mlx.readObjectTempC());
+      //LCD
+
+      //Serial Monitor
+      Serial.print("Temperature: ");
+      Serial.println(mlx.readObjectTempC());
+      Serial.print("Time: ");
+      Serial.println(time);
+      Serial.print("Date: ");
+      Serial.println(date);
+      //Serial Monitor
+
+      //GSM send message
+      sim.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+      delay(200);
+      sim.println("AT+CMGS=\"" + number + "\"\r"); //Mobile phone number to send message
+      delay(200);
+      sim.println("Seth just arrived at school!");
+      sim.println("Time: " + time);
+      sim.println("Date: " + date);       
+      sim.print("Temp: ");
+      sim.print(temp);    
+      sim.println("C");                        
+      delay(100);
+      sim.println((char)26);// ASCII code of CTRL+Z
+      delay(1000);
+      //GSM send message
+
+      //turn on pump
+      digitalWrite(relay, LOW);
+      delay(1500);
+      //turn off pump
+      digitalWrite(relay, HIGH);
+
+      //Data Log
+      //digitalWrite(SS_PIN, HIGH);
+      digitalWrite(CS_SD,LOW);
+      myFile = SD.open("DataLog.txt", FILE_WRITE);
+        if (myFile) 
+        {
+          Serial.println("File opened ok");
+          delay(2000);
+          myFile.print("Name: Seth");   
+          myFile.print(" temp: ");
+          myFile.print(temp);
+          myFile.print(" Time: ");
+          myFile.print(time);
+          myFile.print(" Date: ");
+          myFile.println(date);
+          delay(2000);
+          lcd.clear();
+          myFile.close(); // close the file
+        }
+      else 
+      {
+          Serial.println("error opening Data Log.txt");  
+          myFile.close();
+      }
+    digitalWrite(CS_SD,HIGH);
+      //Data Log
+    delay(2000);
+    Serial.println("Scan RFID");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Scan RFID");
 }
 
+else if (content.substring(1) == "23 BE C0 33") //change here the UID of the card/cards that you want to give access
+  {
+    String number = "09277412790";
+    Serial.println("Card Read and authenticated");
+    //LCD
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Temperature: ");
+    lcd.setCursor(0, 1);
+    lcd.print(mlx.readObjectTempC());
+    //LCD
+
+    //Serial Monitor
+    Serial.print("Temperature: ");
+    Serial.println(mlx.readObjectTempC());
+    Serial.print("Time: ");
+    Serial.println(time);
+    Serial.print("Date: ");
+    Serial.println(date);
+    //Serial Monitor
+
+
+
+    //GSM send message
+    sim.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+    delay(200);
+    //Serial.println ("Set SMS Number");
+    sim.println("AT+CMGS=\"" + number + "\"\r"); //Mobile phone number to send message
+    delay(200);
+    sim.println("Aden just arrived at school!");
+    sim.println("Time: " + time);
+    sim.println("Date: " + date);       
+    sim.print("Temp: ");
+    sim.print(temp);    
+    sim.println("C");                        
+    delay(100);
+    sim.println((char)26);// ASCII code of CTRL+Z
+    delay(1000);
+    //GSM send message
+
+    //turn on pump
+    digitalWrite(relay, LOW);
+    delay(1500);
+    //turn off pump
+    digitalWrite(relay, HIGH);
+
+
+      //Data Log
+
+    digitalWrite(CS_SD,LOW);
+    myFile = SD.open("DataLog.txt", FILE_WRITE);
+      if (myFile) 
+      {
+      Serial.println("File opened ok");
+      delay(2000);
+      myFile.print("Name: Seth");   
+      myFile.print(" temp: ");
+      myFile.print(temp);
+      myFile.print(" Time: ");
+      myFile.print(time);
+      myFile.print(" Date: ");
+      myFile.println(date);
+      delay(2000);
+      lcd.clear();
+      myFile.close(); // close the file
+      }
+    else 
+    {
+      Serial.println("error opening Data Log.txt");  
+      myFile.close();
+    }
+    digitalWrite(CS_SD,HIGH);
+    //Data Log
+    delay(2000);
+    Serial.println("Scan RFID");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Scan RFID");
+
+}
+}
 
 
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # perosnalprojects--attendance-body-temperature-monitoring
 a simple IOT project for monitoring attendance and body temperature with the following modules, RFID,GSM,SD-card,RTC,IR heat sensor, and a relay which will control an alcohol dispenser
+
+-will need for these modules in order to work
+MFRC522 rfid module
+GSM 800L module
+Sd-card module
+arduino uno/nano/mega
+non contact heat sensor (in this case I used a IR heat sensor module)
+real time clock module
+LCD display screen
+1 5v Relay module
+
+all of these libraries are downloadable in the arduino IDE.
